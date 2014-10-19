227f9e98002fe01ce277c3003536e17b985b5b86
Chris Braddock <braddock.chris@gmail.com>
2014-10-19 16:11:38 -0400

    remove unused modules

--- modules/people/manifests/chrisbraddock.pp
+++ modules/people/manifests/chrisbraddock.pp
@@ -1,7 +1,5 @@
 class people::chrisbraddock {
   include webstorm
-  include emacs   # requires emacs module in Puppetfile
-  include sparrow # requires sparrow module in Puppetfile
 
   $home     = "/Users/${::boxen_user}"
   $my       = "${home}/my"
