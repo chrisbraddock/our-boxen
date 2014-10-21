2ff499ceaf480ac5fa54629e2995ade14141eae0
Chris Braddock <braddock.chris@gmail.com>
2014-10-21 17:38:27 -0400

    remove unused node/ruby versions and set global default node ver

--- manifests/site.pp
+++ manifests/site.pp
@@ -64,15 +64,13 @@ node default {
   }
 
   # node versions
-  #include nodejs::v0_6
-  #include nodejs::v0_8
   include nodejs::v0_10
 
+  # set the global nodejs version
+  class { 'nodejs::global': version => 'v0.10.29' }
+
   # default ruby versions
-  ruby::version { '1.9.3': }
-  #ruby::version { '2.0.0': }
-  #ruby::version { '2.1.0': }
-  #ruby::version { '2.1.1': }
+  #ruby::version { '1.9.3': }
   ruby::version { '2.1.2': }
 
   # common, useful packages
